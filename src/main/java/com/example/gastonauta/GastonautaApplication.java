9e75ad060645fd72de49576db051605b07e55ea7
micaela <galeano94mica@gmail.com>
2025-06-23 21:31:11 -0300

    commit inicial

---
+++ src/main/java/com/example/gastonauta/GastonautaApplication.java
@@ -0,0 +1,13 @@
+package com.example.gastonauta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GastonautaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GastonautaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Gastonauta/GastonautaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Gastonauta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GastonautaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
